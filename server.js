318048bc29477feaec0ffcf90eb3c84ee186b084
tassoni13 <tassoni13@gmail.com>
2021-09-15 16:33:16 -0400

    Added path package, updated path for static directory, update port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const path = require("path");
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
